36db04926dff9f5c310b8454954680501c840564
Zherui_Qiu <vibranium@foxmail.com>
2018-06-05 20:05:31 +0800

    Initial commit

---
+++ cal_s.m
@@ -0,0 +1,22 @@
+function S = cal_s(Point)
+
+Point = reshape(Point,[],3);
+DT = delaunayTriangulation(Point);
+[K,~] = convexHull(DT);
+trisurf(K,DT.Points(:,1),DT.Points(:,2),DT.Points(:,3))
+
+[row,~] = size(K);
+convx_p = reshape(DT.Points(K,:),[],3,3);
+convx_eg(:,1) = sqrt(sum((convx_p(:,1,:)-convx_p(:,2,:)).^2,3));
+convx_eg(:,2) = sqrt(sum((convx_p(:,1,:)-convx_p(:,3,:)).^2,3));
+convx_eg(:,3) = sqrt(sum((convx_p(:,2,:)-convx_p(:,3,:)).^2,3));
+convx_eg(:,4) = sum(convx_eg,2)/2;
+Area =sqrt(convx_eg(:,4).*...
+    (convx_eg(:,4)-convx_eg(:,1)).*...
+    (convx_eg(:,4)-convx_eg(:,2)).*...
+    (convx_eg(:,4)-convx_eg(:,3))) ; % Heron's formula
+% p=(a+b+c)/2
+% S =sqrt[p(p-a)(p-b)(p-c)]
+ave = sum(Area)/row;
+S = sum((Area-ave).^2);
+end
---
+++ main.m
@@ -0,0 +1,23 @@
+clear
+Point_Num = 10;
+z_expd = 9;
+Point = zeros(Point_Num,3);
+rng('shuffle')
+Point(:,1) = rand(Point_Num,1)*2*pi;
+Point(:,2) = rand(Point_Num,1);
+Point(1:Point_Num/2,3) = sqrt(z_expd-z_expd*Point(1:Point_Num/2,2).^2);
+Point(Point_Num/2+1:Point_Num,3) = -sqrt(3-3*Point(Point_Num/2+1:Point_Num,2).^2);
+[Point(:,1),Point(:,2)] = pol2cart(Point(:,1),Point(:,2));
+
+options.Algorithm = 'active-set';
+options.Display = 'iter-detailed';
+options.MaxFunctionEvaluations = inf;
+options.MaxIterations = inf;
+options.UseParallel = true;
+Point = fmincon(@cal_s,Point,[],[],[],[],[],[],@mycon,options);
+
+function [c,ceq] = mycon(Point)
+    c = [];
+    ceq = Point(:,1).^2+Point(:,2).^2+Point(:,3).^2/9-1;
+end
+
